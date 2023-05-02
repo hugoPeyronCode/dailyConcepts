788369e3966c938d917e5af3afcec43775df35cf
Hugo Peyron <hugopeyron@gmail.com>
2023-05-02 10:45:42 +0200

    Initial Commit

---
+++ dailyConcept/dailyConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyConceptApp.swift
+//  dailyConcept
+//
+//  Created by Hugo Peyron on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct dailyConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
